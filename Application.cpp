714a5cd1f6b55c9e682643ffe24ca01a95b80e0a
Tempestar <65296828+Tempestar@users.noreply.github.com>
2023-06-16 09:56:49 +0200

    fonction InitialiserCompetition()

--- Application.cpp
+++ Application.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 /// <summary>
-/// Retourne un nombre entier généré au hasard, n,
+/// Retourne un nombre entier genere au hasard, n,
 /// avec deb <= n < fin
 /// </summary>
 /// <param name="deb"> la valeur minimale de n</param>
-/// <param name="fin"> la valeur à laquelle n doit être < </param>
+/// <param name="fin"> la valeur a laquelle n doit etre < </param>
 /// <returns> n avec deb <= n < fin </returns>
 int hasard(int deb, int fin)
 {
@@ -21,36 +21,56 @@ int hasard(int deb, int fin)
 
 
 /// <summary>
-/// Initialiser la compétition.
-/// Consiste à saisir le nombre de dossards nécessaires (= nombre de concurrents)
-/// puis à les créer dans le conteneur dossardsPourAffectation.
+/// Initialiser la competition.
+/// Consiste a saisir le nombre de dossards necessaires (= nombre de concurrents)
+/// puis a les creer dans le conteneur dossardsPourAffectation.
 /// Entre 2 et 100 dossards.
 /// </summary>
 void Application::InitialiserCompetition()
 {
-	//@TODO à compléter.
+    int nombreConcurrents;
+
+    // Demande Ã  l'utilisateur de saisir le nombre de concurrents
+    cout << "Entrez le nombre de concurrents : ";
+    cin >> nombreConcurrents;
+
+    // VÃ©rifie que le nombre de concurrents est valide
+    if (nombreConcurrents < 2 || nombreConcurrents > 100)
+    {
+        cout << "Nombre de concurrents invalide. Le nombre doit Ãªtre compris entre 2 et 100." << endl;
+        return;
+    }
+
+    // GÃ©nÃ¨re les dossards pour chaque concurrent
+    for (int i = 1; i <= nombreConcurrents; i++)
+    {
+        dossardsPourAffectation.push_back(i);
+    }
+
+    cout << "La compÃ©tition a Ã©tÃ© initialisÃ©e avec " << nombreConcurrents << " " << endl;
 }
+
 
 /// <summary>
 /// Inscrire un concurent.
-/// Après vérification qu'il reste un dossard disponible, il faut:
+/// Apres verification qu'il reste un dossard disponible, il faut:
 /// - saisir le nom du concurrent
 /// - retirer 1 dossard au hasard dans le conteneur dossardsPourAffectation
-/// - créer le concurrent avec son nom et le dossard
+/// - creer le concurrent avec son nom et le dossard
 /// - ajouter le concurrent au conteneur concurentsInscrits
 /// Pas d'inscription possible s'il ne reste plus de dossard disponible.
 /// </summary>
 void Application::InscrireUnConcurrent()
 {
-	//@TODO à compléter.
+	//@TODO ï¿½ complï¿½ter.
 }
 
 /// <summary>
-/// Affiche la liste des concurrents inscrits, par ordre alphabétique des noms
+/// Affiche la liste des concurrents inscrits, par ordre alphabï¿½tique des noms
 /// </summary>
 void Application::AfficherParNom()
 {
-	//@TODO à compléter.
+	//@TODO ï¿½ complï¿½ter.
 }
 
 /// <summary>
@@ -58,32 +78,32 @@ void Application::AfficherParNom()
 /// </summary>
 void Application::AfficherParDossard()
 {
-	//@TODO à compléter.
+	//@TODO ï¿½ complï¿½ter.
 }
 
 /// <summary>
 /// Permet de noter (scorer) tous les concurrents.
 /// - extrait et supprime le premier concurrent du conteneur concurentsInscrits
 /// - lui affecte un score entre 0 et 10 inclus
-/// - insère le concurrent noté dans le conteneur resultats, les mieux notés rangés en premier
-/// Le score doit servir de clé pour retrouver tous les concurrents ayant un certain score.
-/// Attention!! On peut avoir plusieurs concurrents avec le même score.
+/// - insere le concurrent note dans le conteneur resultats, les mieux notes ranges en premier
+/// Le score doit servir de cle pour retrouver tous les concurrents ayant un certain score.
+/// Attention!! On peut avoir plusieurs concurrents avec le meme score.
 /// </summary>
 void Application::NoterConcurrents()
 {
-	//@TODO à compléter.
+	//@TODO ï¿½ complï¿½ter.
 }
 
 /// <summary>
-/// Affiche le score, le dossard et le nom des concurrents notés.
+/// Affiche le score, le dossard et le nom des concurrents notï¿½s.
 /// </summary>
 void Application::AfficherResultats()
 {
-	//@TODO à compléter.
+	//@TODO ï¿½ complï¿½ter.
 }
 
 /// <summary>
-/// Boucle d'exécution du programme.
+/// Boucle d'execution du programme.
 /// </summary>
 void Application::Run()
 {
@@ -96,7 +116,7 @@ void Application::Run()
 		menu.Affiche();
 		choix = menu.SaisirEntierEntre(1, 6);
 
-		switch(choix)	//  Réalise l'action choisie par l'utilisateur
+		switch(choix)	//  Realise l'action choisie par l'utilisateur
 		{
 		case 1:
 			InscrireUnConcurrent();
